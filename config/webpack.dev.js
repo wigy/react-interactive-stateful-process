e2596e91a26053a9e26a1674325121f0f8b68815
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-03-29 20:19:55 +0300

    Simplify webpack setup.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,15 +1,74 @@
-const paths = require("./paths");
+const paths = require('./paths');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
 
-module.exports = merge(common, {
-  // Set the mode to development or production
+module.exports = {
+  // Where webpack looks to start building the bundle
+  entry: [paths.src + '/index.js'],
+
   mode: "development",
-
-  // Control how source maps are generated
+  target: 'web',
   devtool: "inline-source-map",
+
+  // Where webpack outputs the assets and bundles
+  output: {
+    path: paths.public,
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
+  },
+
+  // Customize the webpack build process
+  plugins: [
+    // Generates an HTML file from a template
+    new HtmlWebpackPlugin({
+      template: paths.public + '/index.html', // template file
+      favicon: null,
+      filename: 'index.html', // output file
+    }),
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      // JavaScript: Use Babel to transpile JavaScript files
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
+
+      // Styles: Inject CSS into the head with source maps
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1 },
+          },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+
+      // Images: Copy image files to build folder
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      // Fonts and SVGs: Inline files
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 
   // Spin up a server for quick development
   devServer: {
@@ -19,9 +78,4 @@ module.exports = merge(common, {
     hot: true,
     disableHostCheck: true,
   },
-
-  plugins: [
-    // Only update what has changed on hot reload
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-});
+};
