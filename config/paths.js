6136b2a645a222e412dd35f2208e96915792e709
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-03-29 21:11:20 +0300

    Rename example dir.

--- config/paths.js
+++ config/paths.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 module.exports = {
   // Source files
-  src: path.resolve(__dirname, "../sample"),
+  src: path.resolve(__dirname, "../example"),
 
   // Production build files
-  build: path.resolve(__dirname, "../sample/public"),
+  build: path.resolve(__dirname, "../example/public"),
 
   // Static files that get copied to build folder
-  public: path.resolve(__dirname, "../sample/public"),
+  public: path.resolve(__dirname, "../example/public"),
 };
