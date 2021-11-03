e01e2785c703ef540dfb45e66e98dc38d96faeed
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-11-03 19:03:48 +0200

    Add RISPProvider.

--- example/index.tsx
+++ example/index.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from '@material-ui/core';
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { RISPProvider } from '../src';
 import App from './App'
 import theme from './theme'
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}><App /></ThemeProvider>,
+  <RISPProvider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </RISPProvider>,
   document.getElementById('root')
 );
 
