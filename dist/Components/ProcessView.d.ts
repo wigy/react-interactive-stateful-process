9a60d9585fc46472c674821cd7c414a7cdbc359f
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-11-04 19:51:23 +0200

    Start working on step view.

--- dist/Components/ProcessView.d.ts
+++ dist/Components/ProcessView.d.ts
@@ -1,5 +1,16 @@
+export declare type ProcessStepProps = {
+    api: string;
+    proccessId: number;
+    index: number;
+};
+export declare const ProcessStep: (props: ProcessStepProps) => JSX.Element;
 export declare type ProcessViewProps = {
     api: string;
-    id: number;
+    proccessId: number;
 };
+/**
+ * A viewer for single process with step browsing capabilities.
+ * @param props
+ * @returns
+ */
 export declare const ProcessView: (props: ProcessViewProps) => JSX.Element;
