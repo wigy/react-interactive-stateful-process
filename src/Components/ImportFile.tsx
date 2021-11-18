bea6519fd995e014cc04dfebca3741bbd8812771
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-11-18 20:00:52 +0200

    Basic file display.

--- src/Components/ImportFile.tsx
+++ src/Components/ImportFile.tsx
@@ -1,5 +1,21 @@
+import { ExpandMore} from '@material-ui/icons';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@material-ui/core'
 import { TextFileLine } from 'interactive-elements'
 import React from 'react'
+
+export type ImportLineProps = {
+  line: number
+  text: string
+}
+
+export const ImportLine = (props: ImportLineProps): JSX.Element => {
+  return (
+    <TableRow>
+      <TableCell>{props.line}</TableCell>
+      <TableCell><Box sx={{ fontFamily: 'Monospace' }}>{props.text}</Box></TableCell>
+    </TableRow>
+  )
+}
 
 export type ImportFileProps = {
   name: string
@@ -13,10 +29,22 @@ export type ImportFileProps = {
  */
 export const ImportFile = (props: ImportFileProps): JSX.Element => {
 
+  const [expanded, setExpanded] = React.useState(true);
+
   return (
-    <pre>
-      FILE {props.name}
-      LINES {JSON.stringify(props.lines)}
-    </pre>
+      <Accordion expanded={expanded} onChange={() => setExpanded(!expanded)}>
+        <AccordionSummary expandIcon={<ExpandMore />} id={`File ${props.name}`}>
+        <Typography variant="subtitle1"><strong>{props.name}</strong></Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <TableContainer component={Paper}>
+          <Table size="small">
+            <TableBody>
+              {props.lines.map(line => <ImportLine line={line.line + 1} text={line.text}/>)}
+            </TableBody>
+          </Table>
+          </TableContainer>
+      </AccordionDetails>
+    </Accordion>
   )
 }
