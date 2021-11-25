20b7b3c07516961b3af1a13cf161a7dc5447655d
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-11-25 12:13:01 +0200

    Rebuild.

--- dist/Components/DefaultActionView.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type DefaultActionViewProps = {
-    action: Record<string, unknown> | null;
-};
-/**
- * Simple JSON display for action.
- * @param props
- * @returns
- */
-export declare const DefaultActionView: (props: DefaultActionViewProps) => JSX.Element;
--- dist/Components/DefaultDirectionsView.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type DefaultDirectionsViewProps = {
-    directions: Record<string, unknown> | null;
-};
-/**
- * Simple JSON display for directions.
- * @param props
- * @returns
- */
-export declare const DefaultDirectionsView: (props: DefaultDirectionsViewProps) => JSX.Element;
