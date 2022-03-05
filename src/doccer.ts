88e7516ca294d26023c67153d5d452737f59df38
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-05 18:01:10 +0200

    Doc update

--- src/doccer.ts
+++ src/doccer.ts
@@ -4,6 +4,13 @@
  *
  * Visualisation utilities and UI implementation for elements needed in interactive stateful processes.
  *
+ * ## Sub-modules
+ *
+ * * [elements](react_interactive_stateful_process_src_elements.html)
+ * * [misc](react_interactive_stateful_process_src_misc.html)
+ * * [process](react_interactive_stateful_process_src_process.html)
+ * * [risp](react_interactive_stateful_process_src_risp.html)
+ *
  * @module react-interactive-stateful-process
  */
 
