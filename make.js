c174d27d78be8e99466fa1f3464c6815ea7c3579
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-08-27 10:57:25 +0300

    Latest make.js

--- make.js
+++ make.js
@@ -2,20 +2,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const isDev = process.argv.filter(a => a === '--dev').length > 0
 const isBrowser = process.argv.filter(a => a === '--browser').length > 0
+
 const pkg = JSON.parse(require('fs').readFileSync('./package.json'))
 
 async function run() {
   await require('esbuild').build({
     bundle: true,
-    define: {
-      global: 'window'
-    },
+    define: isBrowser ? {
+      global: "window"
+    } : {},
     entryPoints: ['src/index.ts'],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     incremental: isDev,
+    inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
     minify: true,
     outfile: 'dist/index.js',
     platform: isBrowser ? 'browser' : 'node',
+    plugins: isBrowser ? [
+      require('node-stdlib-browser/helpers/esbuild/plugin')(require('node-stdlib-browser'))
+    ] : [
+    ],
     sourcemap: 'external',
     watch: isDev && {
       onRebuild(error, result) {
