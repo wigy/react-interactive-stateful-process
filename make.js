b1815ad70d01f195591f3f827d49c9def962580d
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-08-27 11:07:21 +0300

    Tune builder.

--- make.js
+++ make.js
@@ -1,27 +1,38 @@
 #!/usr/bin/env node
+/**
+ * Requires:
+ *
+ * * esbuild
+ * * node-stdlib-browser
+ */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const isDev = process.argv.filter(a => a === '--dev').length > 0
 const isBrowser = process.argv.filter(a => a === '--browser').length > 0
+const isMinify = process.argv.filter(a => a === '--minify').length > 0
 
 const pkg = JSON.parse(require('fs').readFileSync('./package.json'))
 
 async function run() {
   await require('esbuild').build({
     bundle: true,
-    define: isBrowser ? {
-      global: "window"
-    } : {},
+    define: isBrowser
+      ? {
+          global: 'window'
+        }
+      : {},
     entryPoints: ['src/index.ts'],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     incremental: isDev,
     inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-    minify: true,
+    minify: isMinify,
     outfile: 'dist/index.js',
     platform: isBrowser ? 'browser' : 'node',
-    plugins: isBrowser ? [
-      require('node-stdlib-browser/helpers/esbuild/plugin')(require('node-stdlib-browser'))
-    ] : [
-    ],
+    plugins: isBrowser
+      ? [
+          require('node-stdlib-browser/helpers/esbuild/plugin')(require('node-stdlib-browser'))
+        ]
+      : [
+        ],
     sourcemap: 'external',
     watch: isDev && {
       onRebuild(error, result) {
